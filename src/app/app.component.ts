cc7e6a4bff69bc54185f2e263b2fdb832858cf4c
angelo <angelo1967817162@gmail.com>
2024-11-29 15:45:45 +0100

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet} from '@angular/router';
+import {CookieComponent} from './cookie/cookie.component';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatSidenav, MatSidenavContainer} from '@angular/material/sidenav';
+import {MatListItem, MatNavList} from '@angular/material/list';
+import {MatIcon} from '@angular/material/icon';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from './services/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbar, MatSidenavContainer, MatNavList, MatSidenav, MatListItem, MatIcon, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Chap2Angular';
+  title = 'Angelo';
+  date = new Date();
+  constructor(private auth: AuthService, private http: HttpClient, private router: Router) {
+  }
+  authenticated() { return this.auth.authenticated; }
+  logout(){this.auth.logout();}
 }
