50444597da8db8d99510d8c7b73708ab0190ed39
angelo1967817162 <angelo1967817162@gmail.com>
2024-12-03 16:47:06 +0100

    accueil

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import {MatFormField} from '@angular/material/form-field';
+import {map, Observable, startWith} from 'rxjs';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatAutocomplete,
+  MatAutocompleteOrigin,
+  MatAutocompleteTrigger,
+  MatOption
+} from '@angular/material/autocomplete';
+import {AsyncPipe} from '@angular/common';
+import {MatAnchor, MatButton} from '@angular/material/button';
+import {RouterLink} from '@angular/router';
+import {MatInput} from '@angular/material/input';
+
+@Component({
+  selector: 'app-accueil',
+  imports: [
+    MatFormField,
+    ReactiveFormsModule,
+    MatAutocompleteTrigger,
+    MatAutocomplete,
+    MatOption,
+    AsyncPipe,
+    MatButton,
+    MatAnchor,
+    RouterLink,
+    MatInput,
+    MatAutocompleteOrigin
+  ],
+  templateUrl: './accueil.component.html',
+  styleUrl: './accueil.component.css'
+})
+export class AccueilComponent {
+  myControl = new FormControl('');
+  options: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+  filteredOptions: Observable<string[]>;
+
+  constructor() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || ''))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+}
