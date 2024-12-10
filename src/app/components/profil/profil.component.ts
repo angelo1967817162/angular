c0bc0f7045c09604f106c8637dbf5571372b95ec
cisse000 <salimata2023etude@gmail.com>
2024-12-10 16:33:17 +0100

    pro

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import {MatAnchor} from "@angular/material/button";
+import {MatAnchor, MatButton} from "@angular/material/button";
 import {RouterLink} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-profil',
   imports: [
     MatAnchor,
-    RouterLink
+    RouterLink,
+    MatToolbar,
+    FormsModule,
+    MatButton,
+    MatError,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    ReactiveFormsModule
   ],
   templateUrl: './profil.component.html',
   styleUrl: './profil.component.css'
